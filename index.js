c1050a06080e175d3007d849d18f61880d95ad57
junyug <junyu010988@gmail.com>
2015-03-12 00:24:26 +0800

    add index

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 var express = require('express');
 var app = express();
-
+var fs = require('fs');
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('第一个heroku应用成立!');
+    actions["index"](request, response);
 });
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
+
+var getFile = function (url, callback) {
+    fs.readFile(url, function (err, file) {
+        if (err) {
+            console.log("没有找到对应文件");
+        }
+        callback(file.toString());
+    });
+}
+var actions = {
+    "index": function (req, res) {
+        var file = getFile("index.html", function (file) {
+            res.send(file);
+        });  
+    }
+}
