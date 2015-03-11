187459a51a76c8b15922756e57a59241593dedfb
junyug <junyu010988@gmail.com>
2015-03-11 23:48:58 +0800

    add .gitignore and modify app's name

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('第一个heroku应用成立!');
 });
 
 app.listen(app.get('port'), function() {
