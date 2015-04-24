539a3196a78fec7431e23147793bdf0c5a92c835
junyug <junyu010988@gmail.com>
2015-04-25 00:32:07 +0800

    remove test route

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', routes);
-app.use('/test', tests);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
