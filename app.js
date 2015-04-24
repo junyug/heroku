ca75e4e5e39376c260d682ee97545434dc9b8f71
junyug <junyu010988@gmail.com>
2015-04-25 00:30:24 +0800

    remove test route

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var path = require('path');
 var fs = require('fs');
 
 var routes = require('./routes/index');
-var tests = require('./routes/test');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
